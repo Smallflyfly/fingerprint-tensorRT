814608b77e2db951762320399b6e85f9efe85482
smallflyfly <452639095@qq.com>
2021-01-22 17:02:34 +0800

    添加代码

--- fingerprint_C/main.cpp
+++ fingerprint_C/main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
 #include "argsParser.h"
+#include "buffers.h"
 #include "common.h"
+#include "logger.h"
+#include "parserOnnxConfig.h"
+//
+#include "NvInfer.h"
+#include <cuda_runtime_api.h>
+//
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -9,12 +19,25 @@ class SampleOnnxFingerprint
 {
 	template <typename T>
 	using SampleUniquePtr = unique_ptr<T, samplesCommon::InferDeleter>;
-public:
-	SampleOnnxFingerprint(const samplesCommon::OnnxSampleParams& params) : mParams(params), mEngine(nullptr)
-	{
 
+public:
+	SampleOnnxFingerprint(const samplesCommon::OnnxSampleParams& params) 
+		: mParams(params), mEngine(nullptr)
+	{
 	}
+
+	bool build();
+
+	bool infer();
+
 private:
 	samplesCommon::OnnxSampleParams mParams;
-	shared_ptr<>
+	shared_ptr<nvinfer1::ICudaEngine> mEngine;
+	nvinfer1::Dims mInputDims;
+	nvinfer1::Dims mOutputDims;
+
 };
+
+int main() {
+	cout << "hello" << endl;
+}
